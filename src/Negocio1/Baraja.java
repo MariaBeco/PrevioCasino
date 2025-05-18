b378484cf3d9c777e582abbcbe4042624af02035
USUARIO <USUARIO@DESKTOP-C2RCNRH>
2025-05-18 11:30:36 -0500

    prueba

--- src/Negocio1/Baraja.java
+++ src/Negocio1/Baraja.java
@@ -20,7 +20,9 @@ public class Baraja {
         this.myCartas = new ArrayList<>();
         this.crearCartas();
     }
+public void nada(){
 
+}
     private ArrayList<Carta> crearCartas() {
         String palo[] = {"\u2665", "\u2666", "\u2663", "\u2660"};
         String numero[] = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
