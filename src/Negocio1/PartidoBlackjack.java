bd402f1c271031562adf6a4246c0c82519aa51ab
USUARIO <USUARIO@DESKTOP-C2RCNRH>
2025-05-19 20:10:42 -0500

    Version 8.09n(Aqui se agregan el metodo de saber si hay un blackjack y si es A una carta)

--- src/Negocio1/PartidoBlackjack.java
+++ src/Negocio1/PartidoBlackjack.java
@@ -12,17 +12,19 @@ public class PartidoBlackjack extends Partido {
     private Jugador myGanador;
     private int puntajeGanador;
 
+    
     public PartidoBlackjack( String fecha, Jugador myJugador1, String nombrePartida, int numP, Jugador myJugador2,Baraja barajaU) {
         super( fecha, myJugador1, nombrePartida, numP,myJugador2,barajaU);
        
     }
     
+    
     public String[] enviarCartasInicio(){
         super.getBarajaUsada().barajarCartas(super.getBarajaUsada());
         String []cartasIniciales=new String[4];
-       Juego []juego=super.getMyJuego();
-       Carta [] cartaInicialJug1=new Carta[2];
-       Carta [] cartaInicialJug2=new Carta[2];
+        Juego [] juego=super.getMyJuego();
+        Carta [] cartaInicialJug1=new Carta[2];
+        Carta [] cartaInicialJug2=new Carta[2];
       for(Carta c:super.getBarajaUsada().getMyCartas()){
             for(int i=0;i<4;i++){
                 cartasIniciales[i]=c.toString();
@@ -39,8 +41,31 @@ public class PartidoBlackjack extends Partido {
            }
        return cartasIniciales;
     }
+    
+    
     public String hayBlackjack(){
-        return "";
+        Juego [] juego= super.getMyJuego();
+        int puntaje=0;
+        String cad="";
+        int contAs=0;
+        for(int i=0;i<2;i++){
+            Carta [] cartas=juego[i].getMyCarta();
+            if(this.darleValorAs(cartas[i])==true&&contAs>1){
+                cartas[i].setValor(1);         
+            }
+            puntaje += cartas[i].getValor();
+        }
+        if(puntaje==21)cad="¡Hay BlackJack!";
+        else cad="No hay BlackJack,¡Sigue jugando!:D";
+        return cad;
+    }
+    
+    
+    public boolean darleValorAs(Carta cart){
+        //la primera a siempre vale 11 si hay mas vale 1<----
+        boolean esA=false;
+        if(cart.getValor()==11)esA=true;
+        return esA;
     }
     //inicio juego que devuelve cuatro cartas
 }
